c357da65fb06c6e95ae9ea34b5da0fe5c24f6c25
u2-ow <utwo4805@gmail.com>
2023-09-18 21:35:19 +0900

    タスク追加機能を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,58 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { todo } from 'node:test';
+import React, {  useState,useRef } from 'react'
 
-export default function Home() {
+
+
+
+
+export default function Page() {
+  const [text,setText] = useState('')
+  const [todos,setTodos] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  
+  //inputのテキスト変更を感知して配列に格納
+  const inputChange =(e: { target: { value: React.SetStateAction<string> } })=>{
+    setText(e.target.value)
+  }
+
+  //todoを追加する
+  const addTodo = () =>{
+    if(text.length <= 0){
+      return
+    }
+    console.log(todos)
+    setTodos([...todos, text])
+    //追加後にvalueを空にする
+    setText('')
+  }
+
+  const deleteTodo =()=>{
+    console.log('hello')
+  }
+
+
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+        <h1>本日の予定</h1>
+        <input type="text" value={text} onChange={inputChange} ref={inputRef}  />
+        <button onClick={addTodo}>追加</button>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <h2>やること</h2>
+        <ul>
+          {todos.map((item,index)=>(
+            <li key={index} id={`${index + 1}`}>
+              {item}
+              <button onClick={deleteTodo}>削除</button>
+            </li>
+          ))} 
+        </ul>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    </>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
